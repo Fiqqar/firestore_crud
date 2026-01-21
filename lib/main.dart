10d05ca27639b023a7c5b3445b5e26f37153f324
robotganteng09 <arsyafaisyar@gmail.com>
2026-01-21 11:22:05 +0700

    custome textfield [AMF]

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firestore_crud/routes/pages.dart';
+import 'package:firestore_crud/routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 
 void main() async {
-  await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized(); 
+  // await Firebase.initializeApp();
   runApp(const MainApp());
 }
 
@@ -13,11 +16,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+       initialRoute: AppRoutes.listPage,
+       getPages: AppPages.pages
     );
   }
 }
